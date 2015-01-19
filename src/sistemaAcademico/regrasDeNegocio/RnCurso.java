c16be183c6ea81dcc930023ee5190da86116fcff
Edson Santos <ebsf.si@gmail.com>
2015-01-19 13:16:58 -0300

    Regra de negocio do curso

---
+++ src/sistemaAcademico/regrasDeNegocio/RnCurso.java
@@ -0,0 +1,33 @@
+package sistemaAcademico.regrasDeNegocio;
+
+
+import sistemaAcademico.dao.DaoCurso;
+import sistemaAcademico.dao.DaoCursoInt;
+
+public class RnCurso {
+	
+	DaoCursoInt dao= new DaoCurso();
+	
+	public boolean cursoJaCadastrado(String nome) {
+		boolean jaCadastrado = false;
+		
+		if (dao.consultarTudo().size() != 0) {
+
+			for (int i = 0; i < dao.consultarTudo().size(); i++) {
+				if (nome.equals(dao.consultarTudo().get(i).getNome())) {
+					jaCadastrado = true;
+				}
+			}
+		}
+
+		return jaCadastrado;
+	}
+
+	public boolean semCurso(){
+		boolean vazio=false;
+		if(dao.consultarTudo().size()==0){
+			vazio=true;
+		}
+		return vazio;
+	}
+}
